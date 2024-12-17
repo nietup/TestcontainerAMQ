b8e4f4af86a0a3b0b1e5471c8a848ec43509d10a
Jakub Nietupski <jakubnietupski@drift.oslo.kommune.no>
2024-12-17 13:34:28 +0100

    spring boot setup - no base class

--- src/test/kotlin/no/ok/it/tcproblem/nice/NiceControllerIntegrationTest.kt
+++ src/test/kotlin/no/ok/it/tcproblem/nice/NiceControllerIntegrationTest.kt
@@ -1,16 +1,35 @@
 package no.ok.it.tcproblem.nice
 
-import no.ok.it.tcproblem.config.BaseIntegrationTest
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.testcontainers.activemq.ActiveMQContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
-class NiceControllerIntegrationTest : BaseIntegrationTest() {
+@ActiveProfiles("test")
+class NiceControllerIntegrationTest {
+
+    companion object {
+        @Container
+        @JvmStatic
+        val activeMQContainer: ActiveMQContainer = ActiveMQContainer("apache/activemq-classic:5.18.3")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun amqProperties(registry: DynamicPropertyRegistry) {
+            registry.add("jms.activemq.brokerUrl") { activeMQContainer.brokerUrl }
+        }
+    }
 
     @Autowired
     lateinit var mockMvc: MockMvc
